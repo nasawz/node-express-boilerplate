8ccffeadbc9849b04ea301224ed7f4d1b3054fb4
nasawz <nasa127@gmail.com>
2024-03-17 22:18:16 +0800

    * refactor(ai.controller.js): commented out unused import

--- src/controllers/ai.controller.js
+++ src/controllers/ai.controller.js
@@ -2,7 +2,7 @@ const httpStatus = require('http-status');
 const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
-const { io } = require('../app');
+// const { io } = require('../app');
 const { aiService } = require('../services');
 
 const createMessage = catchAsync(async (req, res) => {
