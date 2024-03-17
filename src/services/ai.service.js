9179fba8f4d5263ea5108c6109d3308d496cc883
nasawz <nasa127@gmail.com>
2024-03-17 20:45:11 +0800

    * feat(ai.service.js): updated chatCompletions function

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -5,18 +5,27 @@ const axios = require('axios');
 const { Fee, User } = require('../models');
 
 const chatCompletions = async (data, user) => {
+  // console.log('chatCompletions');
   return new Promise((resolve, reject) => {
     const userId = user.id;
+    // axios({
+    //   timeout: 5000, // Set a timeout of 5 seconds
+    //   method: 'post',
+    //   url: '/v1/chat/completions',
+    //   baseURL: 'https://api.moonshot.cn',
+    //   headers: {
+    //     authorization: `Bearer ${config.aiApiKey}`,
+    //   },
+    //   data: data,
+    // })
+    data.model = 'gpt-3.5-turbo';
     axios({
-      timeout: 5000, // Set a timeout of 5 seconds
       method: 'post',
-      url: '/v1/chat/completions',
-      baseURL: 'https://api.moonshot.cn',
+      url: 'https://gateway.ai.cloudflare.com/v1/ee4005a384afb2fab9650d18176e56ce/openai/openai/chat/completions',
       headers: {
         authorization: `Bearer ${config.aiApiKey}`,
       },
       data: data,
-      
     })
       .then(function (response) {
         if (response.data.choices.length > 0) {
